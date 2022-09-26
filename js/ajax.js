6380a9e7165b20da4421c670d38a1770168c94d7
andrada <andrada.nistor@gmail.com>
2022-09-26 23:04:21 +0300

    minor details

--- js/ajax.js
+++ js/ajax.js
@@ -1,53 +1,44 @@
 // JavaScript Document
 
-window.onload = function() {
+window.onload = function () {
+  //fetch json
+  document.getElementById("button").onclick = function () {
+    var adr = "https://jsonplaceholder.typicode.com/posts";
 
-    //fetch json
-    document.getElementById("button").onclick = function() {
+    fetch(adr)
+      .then(function (response) {
+        console.log(response);
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Error encountered");
+        }
+      })
+      .then(function (data) {
+        console.log(data);
 
-        var adr = 'https://jsonplaceholder.typicode.com/posts';
-        
-        fetch(adr).then(function(response) {
+        if (data.length) {
+          for (let i of data) {
+            //create a DOM element with obtained data
+            var newItem = document.createElement("tr");
 
-            console.log(response);
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error("Error encountered");
-            }
-        }).then(function(data) {
-            
-            console.log(data);
-            
-            if(data.length) {
-                
-                for(let i of data) {
+            var td1 = document.createElement("td");
+            td1.innerText = i.id;
+            var td2 = document.createElement("td");
+            td2.innerText = i.userId;
+            var td3 = document.createElement("td");
+            td3.innerText = i.title;
 
-                    //create a DOM element with obtained data
-                    var newItem = document.createElement("tr");
+            newItem.appendChild(td1);
+            newItem.appendChild(td2);
+            newItem.appendChild(td3);
 
-                    var td1 = document.createElement("td");
-                    td1.innerText = i.id;
-                    var td2 = document.createElement("td");
-                    td2.innerText = i.userId;
-                    var td3 = document.createElement("td");
-                    td3.innerText = i.title;
-
-                    newItem.appendChild(td1);
-                    newItem.appendChild(td2);
-                    newItem.appendChild(td3);
-
-                    document.querySelector("#table1").appendChild(newItem);
-                }
-            }
-            
-            
-
-        })
-                .catch(function(error) {
-                    alert(error);
-                });
-
-    }
-
-}
+            document.querySelector("#table1").appendChild(newItem);
+          }
+        }
+      })
+      .catch(function (error) {
+        alert(error);
+      });
+  };
+};
